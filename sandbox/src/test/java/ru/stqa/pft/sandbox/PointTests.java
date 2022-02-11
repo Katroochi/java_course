ac7acf73aef1595c1e35ef31b96d0f3879196c68
Zavialova Ekaterina <katroochi@gmail.com>
2022-02-11 18:47:21 +0300

    Доработано Задание 3.

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -3,20 +3,18 @@ package ru.stqa.pft.sandbox;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static ru.stqa.pft.sandbox.MyFirstProgram.distance;
-
 
 public class PointTests {
     @Test
     public void testDistance(){
         Point p1 = new Point(24,50);
         Point p2 = new Point(240,900);
-        Assert.assertEquals(distance(p1,p2), 877.0153932514526);
+        Assert.assertEquals(p1.distance(p2), 877.0153932514526);
     }
     @Test
     public void testDistanceFailure(){
         Point p1 = new Point(24,50);
         Point p2 = new Point(240,900);
-        Assert.assertNotEquals(distance(p1,p2), 877);
+        Assert.assertNotEquals(p1.distance(p2), 877);
     }
 }
