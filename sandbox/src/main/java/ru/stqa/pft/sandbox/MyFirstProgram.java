7870eb717b99ca212f180f53341f33fbd06dfbb2
Zavialova Ekaterina <katroochi@gmail.com>
2022-02-11 18:47:09 +0300

    Выполнено Задание 2.4.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,12 +3,9 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram{
 	public static void main(String[] args){
 		System.out.println("Hello, World!");
-		Point p1 = new Point(24,50);
-		Point p2 = new Point(240,900);
-		System.out.println("Расстояние между первой и второй точкой = " + distance(p1,p2));
+		Point p1 = new Point(204,598);
+     	Point p2 = new Point(2999,7650);
+		System.out.println("Расстояние между первой и второй точкой = " + p1.distance(p2));
     }
 
-	public static double distance(Point p1, Point p2){
-		return Math.sqrt(Math.pow((p2.x - p1.x), 2)+(Math.pow((p2.y - p1.y), 2)));
-	}
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -8,5 +8,11 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-
+    public Point(Point p2){
+        x = p2.x;
+        y = p2.y;
+    }
+    public double distance(Point p2){
+        return Math.sqrt(Math.pow((p2.x- this.x), 2)+(Math.pow((p2.y - this.y), 2)));
+    }
 }
