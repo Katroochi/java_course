67f678bd34dedfa769f9b50cbbac44fd828faa78
Zavialova Ekaterina <katroochi@gmail.com>
2022-03-08 18:09:37 +0300

    Реализованы проверки путём сравнения списков в тестах для создания, удаления и модификации групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,15 +1,30 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
 
 public class GroupCreationTests extends TestBase{
 
   @Test
   public void testGroupCreation(){
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().createNewGroup(new GroupData("group3", "test group3", "test group3"));
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+    GroupData group = new GroupData("group333", "test group3", "test group3");
+    app.getGroupHelper().createNewGroup(group);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    Assert.assertEquals(after.size(),before.size() + 1);
     app.getNavigationHelper().logout();
+
+    before.add(group);
+    Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
