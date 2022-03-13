f0b5322ed9a7ca40c2ec6244cab5b6dd75aa1182
Zavialova Ekaterina <katroochi@gmail.com>
2022-03-13 17:04:19 +0300

    Рефакторинг. Использование уникальных идентификаторов для тестов групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -5,38 +5,34 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupModificationTests extends TestBase{
 
     @BeforeMethod
     public void ensurePreconditions(){
         app.goTo().groupPage();
-        if (app.group().list().size() == 0){
+        if (app.group().all().size() == 0){
             app.group().create(new GroupData().whithName("test2"));
         }
     }
 
     @Test
     public void testGroupModification() {
-        List<GroupData> before = app.group().list();
-        int index = before.size() - 1;
+        Set<GroupData> before = app.group().all();
+        GroupData modifiedGroup = before.iterator().next();
         GroupData group = new GroupData()
-                .whithId(before.get(index).getId())
+                .whithId(modifiedGroup.getId())
                 .whithName( "group3")
                 .whithHeader( "test group3")
                 .whithFooter( "test group3");
-        app.group().modify(index, group);
-        List<GroupData> after = app.group().list();
+        app.group().modify(group);
+        Set<GroupData> after = app.group().all();
         Assert.assertEquals(after.size(),before.size());
         app.goTo().logout();
 
-        before.remove(index);
+        before.remove(modifiedGroup);
         before.add(group);
-        Comparator<? super GroupData> byId = Comparator.comparingInt(GroupData::getId);
-        before.sort(byId);
-        after.sort(byId);
         Assert.assertEquals(before, after);
     }
 
